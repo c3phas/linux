5e0767567dd9844d648bbc7eafac756c2ab9379f
peter macharia <mainapeter081@gmail.com>
2018-11-29 07:57:06 +0300

    create a symbolic link

---
+++ link.c
@@ -0,0 +1,39 @@
+/*This will be used to create a link for a file *
+ * use gcc -o output link.c to compile the file *
+ *allows you to link one file to multiple paths *
+ * the command should only be run in  unix/linux*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main( int argc, char **argv)
+{
+   char *old_path= (char *)malloc(40*sizeof(char));
+   char *new_path= (char *)malloc(30*sizeof(char));
+
+   printf("Enter the path of the file you wish to link\n");
+  // fflush(stdin);
+   scanf(" %s",old_path);
+   fflush(stdin);
+   printf("Enter the new path to create\n");
+  // fflush(stdin);
+   scanf(" %s",new_path);
+	printf(" %s",new_path);
+   int link_response;
+   link_response = symlink(old_path,new_path);
+	   if (link_response ==-1){
+			//fprintf(stderr,"an error occured");
+			perror("link");
+			exit(1);
+	   }else{
+   
+		printf("Successfully created the link");
+		exit(0);
+	   }
+   
+   
+   
+
+   
+	return 0;
+}
