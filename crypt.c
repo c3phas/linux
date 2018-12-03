e1d4c7a8c7605e9bf7b77e070fa6ee772ed1bf85
peter macharia <mainapeter081@gmail.com>
2018-12-03 22:02:32 +0300

    an encryption program

---
+++ crypt.c
@@ -0,0 +1,24 @@
+/*implementing the crypt algorithm/function *
+ *which uses DES as the encryption algorithm *
+ *The program takes two args the password and salt*
+ *to compile it just type gcc -o output_file crypt.c -lcrypt
+ *then run the executable named (output_file)
+ *
+ * */
+#include <stdio.h>
+#include <unistd.h>
+#include <crypt.h>
+int main(int argc, char **argv)
+{
+	if (argc == 3){
+		printf("%s\n",crypt(argv[1],argv[2]));
+	
+	}else{
+	
+		printf("usage :crypt password salt\n");
+	
+	}
+
+
+return 0;
+}
